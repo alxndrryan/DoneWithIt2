a0cc9618889940fd6fb8bcb7b4afbf35db49990a
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-28 16:55:36 -0500

    builds basic welcome screen

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { useDeviceOrientation } from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-      }}
-    >
-      <View style={{ backgroundColor: "dodgerblue", flex: 1 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
