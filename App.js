c1999e0e93f25ea31d3ec1fdb774a6009e0911e5
Rand Al'Thor <alexanderhogle@gmail.com>
2022-06-28 17:08:06 -0500

    builds basic view image screen

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { useDeviceOrientation } from "@react-native-community/hooks";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
