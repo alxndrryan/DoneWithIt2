d7b885cdd9fb2d62a23de67340cb97a43614f2bc
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 09:55:13 -0500

    builds card component

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { useDeviceOrientation } from "@react-native-community/hooks";
 
-import AppText from "./app/components/AppText";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
