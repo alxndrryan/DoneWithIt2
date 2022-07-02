ff7bccb2ad24b3b9de79ca6ebd4fb6ee86cb6524
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 15:16:07 -0500

    builds picker component

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
-import { Text, TextInput } from "react-native";
+import { Text, TextInput, Switch } from "react-native";
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [isNew, setIsNew] = useState(false);
+
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email" />
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
