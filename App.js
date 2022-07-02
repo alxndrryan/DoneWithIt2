b68e8116cff52d8f8880c4653b6c346f918d06cd
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 10:14:07 -0500

    builds listing details screen

--- App.js
+++ App.js
@@ -2,22 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { useDeviceOrientation } from "@react-native-community/hooks";
 
-import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card
-        title="Red jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
