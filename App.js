e8bd7054a20f97d97a3ec27791da6af9777c67b3
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 13:42:14 -0500

    builds the account screen

--- App.js
+++ App.js
@@ -8,15 +8,8 @@ import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
-  return (
-    <Screen>
-      <ListItem
-        title="My title"
-        subTitle="My subtitle"
-        ImageComponent={<Icon name="email" />}
-      />
-    </Screen>
-  );
+  return <AccountScreen />;
 }
