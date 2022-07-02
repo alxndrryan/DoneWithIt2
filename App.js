ee6ffe0ec20c457ce4eae7932521050f35a550ab
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 11:06:10 -0500

    builds messages screen

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { useDeviceOrientation } from "@react-native-community/hooks";
 
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
