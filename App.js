6d7ebffc50fcf85ecd62e55e5d4ac2c499c90795
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 14:51:40 -0500

    builds stylized text input component

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { useDeviceOrientation } from "@react-native-community/hooks";
+import { useState } from "react";
 
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import { Text, TextInput } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email" />
+    </Screen>
+  );
 }
