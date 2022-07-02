8aa7ae8c522fde70be9d4ddced6151ca29d841fd
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 10:30:35 -0500

    builds list item component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -3,6 +3,7 @@ import { Image, StyleSheet, View } from "react-native";
 import AppText from "../components/AppText";
 
 import colors from "../config/colors";
+import ListItem from "../components/ListItem";
 
 function ListingDetailsScreen(props) {
   return (
@@ -11,6 +12,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/me.jpeg")}
+            title="Alexander Hogle"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -33,6 +41,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
