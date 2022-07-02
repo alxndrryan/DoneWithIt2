b37913744214383df56b4cc538db74d207a62df5
Alexander Hogle <alexanderhogle@gmail.com>
2022-07-02 17:49:54 -0500

    builds listing edit screen

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 import LoginScreen from "./app/screens/LoginScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <ListingEditScreen />;
 }
