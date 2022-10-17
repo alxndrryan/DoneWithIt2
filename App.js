6a777add122b1608405f35253b08dc49c51526fd
Alexander Hogle <alexanderhogle@gmail.com>
2022-10-17 18:49:00 -0500

    get users location when listing item / adds categorypickeritem onpress event

--- App.js
+++ App.js
@@ -13,23 +13,5 @@ import Screen from './app/components/Screen';
 import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
-
-  return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import Text from "./Text";
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Text from './Text';
 
-import Icon from "./Icon";
+import Icon from './Icon';
 
 function CategoryPickerItem({ item, onPress }) {
   return (
     <View style={styles.container}>
-      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
-      <Text style={styles.label}>{item.label}</Text>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+        <Text style={styles.label}>{item.label}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,13 +23,13 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 30,
     paddingVertical: 15,
-    alignItems: "center",
-    width: "33%",
+    alignItems: 'center',
+    width: '33%'
   },
   label: {
     marginTop: 5,
-    textAlign: "center",
-  },
+    textAlign: 'center'
+  }
 });
 
 export default CategoryPickerItem;
