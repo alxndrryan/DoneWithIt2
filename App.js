15c5b8e5f887141578ed82241b4aa5920b30be30
Alexander Hogle <alexanderhogle@gmail.com>
2022-10-31 12:44:34 -0500

    customizing headers example

--- App.js
+++ App.js
@@ -30,8 +30,21 @@ const TweetDetails = ({ route }) => (
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets} />
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: 'dodgerblue' },
+      headerTintColor: 'white'
+    }}
+  >
+    <Stack.Screen
+      name="Tweets"
+      component={Tweets}
+      options={{
+        headerStyle: { backgroundColor: 'tomato' },
+        headerTintColor: 'white',
+        headerShown: false
+      }}
+    />
     <Stack.Screen
       name="TweetDetails"
       component={TweetDetails}
