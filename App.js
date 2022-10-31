2e5eaae93e56d253df33153ab5a8adf1beeca968
Alexander Hogle <alexanderhogle@gmail.com>
2022-10-31 12:25:35 -0500

    hooks up stack navigator

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
-import { Button, Image } from 'react-native';
-
-import * as ImagePicker from 'expo-image-picker';
-import * as Permissions from 'expo-permissions';
-
-import ListingEditScreen from './app/screens/ListingEditScreen';
-import LoginScreen from './app/screens/LoginScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ImageInputList from './app/components/ImageInputList';
+import { Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Screen from './app/components/Screen';
-import { useEffect, useState } from 'react';
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>TweetDetails</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
