4cc44c715f5267c2fb52d751e4b9a04778efce31
Alexander Hogle <alexanderhogle@gmail.com>
2022-12-01 18:38:53 -0600

    adds appnavigator

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Screen from './app/components/Screen';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import AppNavigator from './app/navigation/AppNavigator';
 
 const Link = () => {
   const navigation = useNavigation();
@@ -74,7 +75,7 @@ const TabNavigator = () => (
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
