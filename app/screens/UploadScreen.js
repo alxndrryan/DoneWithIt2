9585b09bf510f9850174cbf893f27d06cf13abff
Alexander Hogle <alexanderhogle@gmail.com>
2022-12-20 17:39:24 -0600

    adds progress bar with react-native-progress

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Modal, View } from 'react-native';
-import Text from '../components/Text';
+import * as Progress from 'react-native-progress';
+
+import colors from '../config/colors';
 
 function UploadScreen({ progress = 0, visible = false }) {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Text>{progress * 100}%</Text>
+        <Progress.Bar color={colors.primary} progress={progress} width={200} />
       </View>
     </Modal>
   );
