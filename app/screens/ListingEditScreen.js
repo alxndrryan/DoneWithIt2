9ae72e97cec8b5f9b8d57a5c930bf01104bcc2a2
Alexander Hogle <alexanderhogle@gmail.com>
2022-12-19 20:04:14 -0600

    displays progress screen with using text. needs progress bar

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
 
@@ -13,6 +14,7 @@ import {
   SubmitButton
 } from '../components/forms';
 import Screen from '../components/Screen';
+import UploadScreen from './UploadScreen';
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label('Title'),
@@ -81,18 +83,24 @@ const categories = [
 
 function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const handleSubmit = async (listing) => {
+    setUploadVisible(true);
     const result = await listingsApi.addListing(
       { ...listing, location },
-      (progress) => console.log(progress)
+      (progress) => setProgress(progress)
     );
+    setUploadVisible(false);
+
     if (!result.ok) return alert('Could not save the listing.');
     alert('Success');
   };
 
   return (
     <Screen style={styles.container}>
+      <UploadScreen progress={progress} visible={uploadVisible} />
       <Form
         initialValues={{
           title: '',
